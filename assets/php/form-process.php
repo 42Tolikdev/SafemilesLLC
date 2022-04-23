4d9cfe7ae4c96994d9bf4dba5605a364c4d3ea9b
Tulkinjon <42tolikdev@gmail.com>
2022-04-23 11:40:01 -0500

    email php

--- assets/php/form-process.php
+++ assets/php/form-process.php
@@ -1,43 +1,7 @@
 <?php
 
 $errorMSG = "";
-
-// NAME
-if (empty($_POST["name"])) {
-    $errorMSG = "Name is required ";
-} else {
-    $name = $_POST["name"];
-}
-
-// EMAIL
-if (empty($_POST["email"])) {
-    $errorMSG .= "Email is required ";
-} else {
-    $email = $_POST["email"];
-}
-
-// MSG SUBJECT
-if (empty($_POST["msg_subject"])) {
-    $errorMSG .= "Subject is required ";
-} else {
-    $msg_subject = $_POST["msg_subject"];
-}
-
-// Phone Number
-if (empty($_POST["phone_number"])) {
-    $errorMSG .= "Number is required ";
-} else {
-    $phone_number = $_POST["phone_number"];
-}
-
-// MESSAGE
-if (empty($_POST["message"])) {
-    $errorMSG .= "Message is required ";
-} else {
-    $message = $_POST["message"];
-}
-
-$EmailTo = "42tolikdev@gmail.com";
+$EmailTo = "hrsafemiles@gmail.com";
 
 $Subject = "New Message Received";
 
@@ -72,5 +36,7 @@ if ($success && $errorMSG == ""){
         echo $errorMSG;
     }
 }
+//redirect to success page
+header('Location: success.html');
 
 ?>
